96f576986cee12f7fcbc99d1bdecd6c71837a0fb
Joao Lucas <jldecristo@gmail.com>
2025-08-04 21:39:51 -0300

    getters

--- src/application/entities/Product.java
+++ src/application/entities/Product.java
@@ -13,4 +13,12 @@ public class Product {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
 }
