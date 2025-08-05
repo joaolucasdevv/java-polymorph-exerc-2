18becca08d6cb74ff42c1a15214c6f7f19ccc9bc
Joao Lucas <jldecristo@gmail.com>
2025-08-04 22:02:26 -0300

    construtores

--- src/application/entities/UsedProduct.java
+++ src/application/entities/UsedProduct.java
@@ -2,8 +2,16 @@ package application.entities;
 
 import java.time.LocalDate;
 
-public class UsedProduct {
+public class UsedProduct extends Product{
 
     private LocalDate manufactureDate;
+
+    public UsedProduct() {
+    }
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
 
 }
