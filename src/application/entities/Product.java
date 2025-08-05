6890c2d2c4b9ef2eedb4a0925571e5a0b7b7b5be
Joao Lucas <jldecristo@gmail.com>
2025-08-04 21:38:55 -0300

    contrutores

--- src/application/entities/Product.java
+++ src/application/entities/Product.java
@@ -5,5 +5,12 @@ public class Product {
     private String name;
     private Double price;
 
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
 
 }
